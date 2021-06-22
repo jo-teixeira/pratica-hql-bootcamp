284fa265ae75ae58d2d648369e732e605fe01caf
jo-teixeira <joao.teixeira@mercadolivre.com>
2021-06-22 10:03:47 -0300

    initialize project

---
+++ src/main/java/com/mercadolivre/PraticaHQL/PraticaHqlApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.PraticaHQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaHqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaHqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolivre/PraticaHQL/PraticaHqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.PraticaHQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaHqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
